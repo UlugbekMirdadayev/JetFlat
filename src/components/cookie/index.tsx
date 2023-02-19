be65206c9c14b97de971059f0cc08474130b8d3e
NomonovFarhod <farhodbek.nomonov@mail.ru>
2023-02-19 23:01:25 +0500

    update src/components/cookie/index.tsx and src/components/popups/foxModal/index.tsx

--- src/components/cookie/index.tsx
+++ src/components/cookie/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button } from '../button';
 import styles from './style.module.scss';
 
 export const Cookie = () => {
-  const [noneBlock, setNoneBlock] = React.useState<Boolean>(true);
+  const [active, setActive] = useState<Boolean>(true);
   return (
     <>
-      {noneBlock ? (
+      {active ? (
         <div className={styles.cookie}>
           <div className={styles.cookie__inner}>
             <span className={styles.cookie__text}>Мы используем файлы cookie</span>
             <a href="/" className={styles.cookie__link}>
               Соглашение об использовании
             </a>
-            <Button variant="primary" onClick={() => setNoneBlock(false)}>
+            <Button variant="primary" onClick={() => setActive(false)}>
               Принять
             </Button>
           </div>

36db969cbd678a0b9b394218fc16d3951e7603ee
FrontendDevoloper <ulugbekmirdadayev1211@gmail.com>
2023-02-19 19:49:10 +0500

    update src/components/cookie/index.tsx, style.module.scss, Popular.tsx and style.module.scss

--- src/components/cookie/index.tsx
+++ src/components/cookie/index.tsx
@@ -3,15 +3,24 @@ import { Button } from '../button';
 import styles from './style.module.scss';
 
 export const Cookie = () => {
+  const [noneBlock, setNoneBlock] = React.useState(true);
   return (
-    <div className={styles.cookie}>
-      <div className={styles.cookie__inner}>
-        <span className={styles.cookie__text}>Мы используем файлы cookie</span>
-        <a href="/" className={styles.cookie__link}>
-          Соглашение об использовании
-        </a>
-        <Button variant="primary">Принять</Button>
-      </div>
-    </div>
+    <>
+      {noneBlock ? (
+        <div className={styles.cookie}>
+          <div className={styles.cookie__inner}>
+            <span className={styles.cookie__text}>Мы используем файлы cookie</span>
+            <a href="/" className={styles.cookie__link}>
+              Соглашение об использовании
+            </a>
+            <Button variant="primary" onClick={() => setNoneBlock(false)}>
+              Принять
+            </Button>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
   );
 };
